578c9efa22fcb2731595b9393c23dda56f3d0d2f
Akello mary <akellomary665@gmail.com>
2025-02-21 11:07:48 +0300

    mary num6 and num7

---
+++ index.js
@@ -0,0 +1,5 @@
+// 6. Write a JavaScript function that converts a temperature from Celsius to Fahrenheit. The formula is:
+// F=(C×9/5)+32.The user should enter a value in Celsius, and the result should be displayed in Fahrenheit.
+
+// 7. Write a JavaScript function that takes a string from the user and reverses it. 
+// Display the reversed string in an HTML paragraph.
